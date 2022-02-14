4d49f461f752d08cc7fe628e05a396d7f85cb0a6
PM-00-STAR <pratibhamitra01@gmail.com>
2022-02-14 20:34:58 +0530

    commit

---
+++ src/pages/Typography.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+function Create() {
+  return (
+    <div>
+        <Typography variant="h1" color="primary" align="center">
+            Create a new note
+        </Typography>
+    </div>
+  )
+}
+
+export default Create
